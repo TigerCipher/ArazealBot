6d64ea4f261b0df71ce3d37fc30f4114433ff0f3
TigerCipher <67520209+TigerCipher@users.noreply.github.com>
2024-03-02 21:52:01 -0600

    testing command extension

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import os
 import discord
+from discord.ext import commands
 
 from dotenv import load_dotenv
 
@@ -9,11 +10,28 @@ TOKEN = os.getenv('TOKEN')
 intents = discord.Intents.default()
 intents.typing = False
 intents.presences = False
+intents.message_content = True
 
-client = discord.Client(intents=intents)
+# client = discord.Client(intents=intents)
+client = commands.Bot(command_prefix='$', intents=intents)
+
+
+@client.command(name='foo', help='test command')
+async def foo(ctx, arg):
+    await ctx.send(arg)
+
 
 @client.event
 async def on_ready():
     print(f'{client.user} has connected to Discord!')
+
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    await client.process_commands(message)
+
 
 client.run(TOKEN)
