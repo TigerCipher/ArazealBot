45d58c041d83fa1ced89331a614bc835570a0b4b
TigerCipher <67520209+TigerCipher@users.noreply.github.com>
2024-03-04 00:28:18 -0600

    added create command

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-import json
 import os
 import discord
 from discord.ext import commands
@@ -23,8 +22,6 @@ client = commands.Bot(command_prefix='$', intents=intents)
 @client.command(help='roll a dice of a given type')
 async def roll(ctx, dice_type: str = commands.parameter(description='d4, d6, d8, d10, d12, d20, d100'),
                num_rolls: int = commands.parameter(description='Number of dice to roll', default=1)):
-    result = 0
-    rolls = []
     try:
         result, rolls = rpg.roll_dice(dice_type, int(num_rolls))
     except ValueError:
@@ -34,6 +31,32 @@ async def roll(ctx, dice_type: str = commands.parameter(description='d4, d6, d8,
         await ctx.send(f'You rolled a {result} from rolls: {rolls}')
     else:
         await ctx.send(f'You rolled a {result}')
+
+
+@client.command(help='Create a character to roleplay in the world of Arazeal')
+async def create(ctx, *name: str):
+    if name is None or len(name) == 0:
+        await ctx.send('You must supply a name')
+        return
+    user_id = ctx.message.author.id
+    c = entity.load_character_by_id(user_id)
+    if c is not None:
+        await ctx.send(f'You already have a character ({c.name}). You can delete it with `$restart`')
+    else:
+        full_name = ' '.join(name)
+        _ = entity.Character(full_name, 1, 10, 10, 1, 1, 0, 10, 10, [], user_id)
+        entity.save_all_characters()
+        await ctx.send(f'Your character "{full_name}" has been created')
+
+
+@client.command(help='test')
+async def fake(ctx):
+    user_id = ctx.message.author.id
+    c = entity.load_character_by_id(user_id)
+    if c is not None:
+        await ctx.send(f'{c.name} has used the fake power')
+    else:
+        await ctx.send('You must first create a character with `$create`')
 
 
 @client.event
@@ -49,23 +72,10 @@ async def on_message(message):
     await client.process_commands(message)
 
 
-#testPlayer = entity.Character('Billy the Kid', 2, 10, 10, 5, 6, 10, 100,
-#                              15, ['sword', 'healing potion', 'rope'], 4353465654645)
-# testPlayer2 = entity.Character('John', 5, 20, 20, 10, 5, 100, 1000,
-#                               35, ['bow', '20 arrows', 'dagger'], 54563456)
-
-# print('Testing json saving')
-# entity.save_all_characters()
-
 print('Testing json reading')
 loadedPlayer = entity.load_character_by_id(54563456)
 loadedPlayer.test_print_info()
 
-# testPlayer3 = entity.Character('Cynthia', 5, 5, 5, 5, 5, 5, 55, 5, ['apple'], 234)
-# print('Saving 3rd character')
-# entity.save_all_characters()
-
-# testPlayer3.gold = 65
 loadedPlayer.xp += 5
 
 print('Testing saving after modifications')
